d00fdda03ae665c0321c12f7ea0a3346293d32d4
Muhammad Harisz Ryadi <harisz.ryadi17@gmail.com>
2024-06-30 09:05:37 +0700

    update route mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('/profile', [ProfileController::class, 'index']);
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
