5489ed180f44781964f3feed3472f8aef025a6e5
Muhammad Harisz Ryadi <harisz.ryadi17@gmail.com>
2024-08-18 07:05:56 +0700

    menambahkan autentikasi

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -339,11 +339,15 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Logout</a>
                 </div>
             </div>
         </div>
     </div>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
+        @csrf
+    </form>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
